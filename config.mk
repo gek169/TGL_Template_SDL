8f52634ace1d5a8b98861db593481bf146708658
gek169 <gek169>
2021-05-25 13:49:41 -0400

    Automatic commit.

--- config.mk
+++ config.mk
@@ -8,15 +8,15 @@ CC= clang
 
 
 #For GCC/Clang on good OSes:
-SDL_LIBS= -lSDL 
+SDL_LIBS= -lSDL -lm
 #for MinGW users:
 #SDL_LIBS= -lmingw32 -lSDLmain -lSDL 
 
 SDL_MIXERLIBS= -lSDL_mixer -lmad -logg -lmikmod -logg
 
 #extract the maximum possible performance from gcc.
-CFLAGS= -Wall -Ofast -std=c99 -DNDEBUG -fopenmp -mtune=native -Wno-unused-function -lm
-CFLAGS_LIB= -Wall -Ofast -std=c99 -pedantic -DNDEBUG -fopenmp -mtune=native -Wno-unused-function -lm
+CFLAGS= -Wall -Ofast -std=c99 -DNDEBUG -fopenmp -mtune=native -Wno-unused-function 
+CFLAGS_LIB= -Wall -Ofast -std=c99 -pedantic -DNDEBUG -fopenmp -mtune=native -Wno-unused-function 
 #CFLAGS= -Wall -Ofast -std=gnu99 -DNDEBUG -mtune=native
 #uncomment the following line for compiling the lib with tinyc
 #keep these flags for compiling with gcc if you are linking with tinygl compiled with tcc.

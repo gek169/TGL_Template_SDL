0746d8e94beea8df0434bf6c0d10b9255ec22e52
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-06 01:46:51 -0400

    Automatic commit.

--- config.mk
+++ config.mk
@@ -16,8 +16,12 @@ SDL_LIBS= -lSDL -lm
 SDL_MIXERLIBS= -lSDL_mixer -lmad -logg -lmikmod -logg
 
 #extract the maximum possible performance from gcc.
-CFLAGS= -Wall -Ofast -std=c99 -DNDEBUG -fopenmp -mtune=native -Wno-unused-function 
-CFLAGS_LIB= -Wall -Ofast -std=c99 -pedantic -DNDEBUG -fopenmp -mtune=native -Wno-unused-function 
+#CFLAGS= -Wall -Ofast -std=c99 -DNDEBUG -fopenmp -march=native -Wno-unused-function 
+CFLAGS= -Wall -O3 -std=c99 -DNDEBUG -march=native -Wno-unused-function 
+#CFLAGS_LIB= -Wall -Ofast -std=c99 -pedantic -DNDEBUG -fopenmp -march=native -Wno-unused-function 
+CFLAGS_LIB= -Wall -O3 -std=c99 -pedantic -DNDEBUG -march=native -Wno-unused-function 
+
+
 #CFLAGS= -Wall -Ofast -std=gnu99 -DNDEBUG -mtune=native
 #uncomment the following line for compiling the lib with tinyc
 #keep these flags for compiling with gcc if you are linking with tinygl compiled with tcc.

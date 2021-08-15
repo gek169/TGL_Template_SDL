53032de54bc9f82f2de5d08fff7fc3715f99a29d
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-08-15 13:53:37 -0400

    gitap from gek@Katherine.

--- src_tgl/zmath.h
+++ src_tgl/zmath.h
@@ -60,7 +60,7 @@ static GLfloat fastInvSqrt(GLfloat x) {
 	GLint i;
 	GLfloat x2;
 	memcpy(&i, &x, 4);
-	i = 0x5F1FFFF9 - (i >> 1);
+	i = 0x5f3759df - (i >> 1);
 	memcpy(&x2, &i, 4);
 	x2 *= 0.703952253f * (2.38924456f - x * x2 * x2);
 	return x2;
